76e1efd333dcafb3afe2e7915886a5ebb9498144
unknown <ruslanchik_nadyrov@mail.ru>
2024-09-30 00:37:31 +0600

    fix: mathStars func

--- src/entities/products/lib/mathStars.ts
+++ src/entities/products/lib/mathStars.ts
@@ -1,11 +1,8 @@
 import { type TMathStars } from '@/entities/products'
 
 export const mathStars = (rating: number): TMathStars => {
-  let starRating
-  let unStarRating
-
-  starRating = Math.round((rating / 100) * 5)
-  unStarRating = 5 - starRating
+  let starRating = Math.round((rating / 100) * 5)
+  let unStarRating = 5 - starRating
 
   return { starRating, unStarRating }
 }
