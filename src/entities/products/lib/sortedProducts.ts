b1b17b1a64e65954ba9eb881031a461f24e6b9f4
unknown <ruslanchik_nadyrov@mail.ru>
2024-10-13 16:00:38 +0600

    fix: lib sorted products

--- src/entities/products/lib/sortedProducts.ts
+++ src/entities/products/lib/sortedProducts.ts
@@ -2,33 +2,33 @@ import type { TProduct } from '@/shared/types'
 import { mathSalePrice } from './mathSalePrice'
 import type { SortType, TPrice } from '../model'
 
+const getPivotValue = (product: TProduct, sortType: SortType): number | string => {
+  switch (sortType) {
+    case 'byPopularity':
+      return product.rating
+    case 'beforeCheaper':
+    case 'beforeMoreExpensive': {
+      const productPrice: TPrice = { price: product.price, discount: product.discount }
+      return mathSalePrice(productPrice)
+    }
+    case 'beforeNew':
+      return new Date(product.createdAt).getTime()
+    case 'byAlphabet':
+      return product.title
+    default:
+      return ''
+  }
+}
+
 export const quickSort = (arr: TProduct[], sortType: SortType): TProduct[] => {
   if (arr.length <= 1) return arr
 
-  const getPivotValue = (product: TProduct): number | string => {
-    switch (sortType) {
-      case 'byPopularity':
-        return product.rating
-      case 'beforeCheaper':
-      case 'beforeMoreExpensive': {
-        const productPrice: TPrice = { price: product.price, discount: product.discount }
-        return mathSalePrice(productPrice)
-      }
-      case 'beforeNew':
-        return new Date(product.createdAt).getTime()
-      case 'byAlphabet':
-        return product.title
-      default:
-        return ''
-    }
-  }
-
-  const pivot = getPivotValue(arr[arr.length - 1])
+  const pivot = getPivotValue(arr[arr.length - 1], sortType)
   const left: TProduct[] = []
   const right: TProduct[] = []
 
   for (let i = 0; i < arr.length - 1; i++) {
-    const currentValue = getPivotValue(arr[i])
+    const currentValue = getPivotValue(arr[i], sortType)
 
     if (
       sortType === 'beforeMoreExpensive' || sortType === 'byPopularity' || sortType === 'beforeNew'
